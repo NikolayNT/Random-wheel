d18c80bb791c027ec2f773c8c17d84723a58f53d
Nikolay Dolgov <koly201020@yandex.ru>
2024-03-03 16:18:14 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="title">
+    <img src="./img/logo.png" class="title__img">
+  </header>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Verdana, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  padding-left: 50px;
+}
+
+.title {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  height: 100px;
+  margin-top: 20px;
+  margin-bottom: 50px;
+}
+
+.title__img {
+  position: absolute;
+  left: 55px;
+  top: 0;
+  height: 100px;
+}
+</style>
